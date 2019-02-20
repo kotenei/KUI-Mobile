57b09834a51a60202a5bd13a6153b5a413adaa5d
hutianrong <tianrong.x.hu@ericsson.com>
2019-02-20 17:39:35 +0800

    add files

---
+++ scripts/build.js
@@ -0,0 +1,5 @@
+const glob = require("glob");
+const chalk = require("chalk");
+const rimraf = require("rimraf");
+const shell = require("shelljs");
+const cwd = process.cwd();
---
+++ site/index.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.scss';
+
+const App = () => {
+  return <div className="k-mobile-site">KUI-Mobile</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
