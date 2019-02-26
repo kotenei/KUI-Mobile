3c4396f85f705f0c927bf65e6e2c5e5f6df4e4d2
kotenei <kotenei@qq.com>
2019-02-26 23:59:03 +0800

    add files

---
+++ src/components/View/View.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { ViewProps, ViewState } from './typing';
+
+export default class View extends Component<ViewProps, ViewState> {
+  public render() {
+    return null;
+  }
+}
---
+++ src/components/View/typing.ts
@@ -0,0 +1,3 @@
+export interface ViewProps extends KUI.BaseProps {}
+
+export interface ViewState {}
