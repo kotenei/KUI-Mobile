4711f81b843936e333e40b67b98dbc9c6ac03905
kotenei <kotenei@qq.com>
2019-03-06 00:06:57 +0800

    update

--- src/components/Collapse/Collapse.tsx
+++ src/components/Collapse/Collapse.tsx
@@ -1,9 +1,59 @@
 import React, { PureComponent } from 'react';
+import classnames from 'classnames';
+import { CollapseProps, CollapseState } from './typing';
 
-class Collapse extends PureComponent {
+const prefixCls = 'k-collapse';
+
+class Collapse extends PureComponent<CollapseProps, CollapseState> {
+  private static defaultProps = {
+    defaultActiveIds: [],
+  };
+  public state = {
+    activeIds: this.props.activeIds || this.props.defaultActiveIds,
+  };
   public render() {
-    return null;
+    const { children, className } = this.props;
+    const { activeIds } = this.state;
+    const classString = classnames(
+      {
+        [prefixCls]: true,
+      },
+      className,
+    );
+    return (
+      <div className={classString}>
+        {React.Children.map(children, (child: any, index) => {
+          if (!child || !child.type || child.type.displayName !== 'CollapsePanel') {
+            return null;
+          }
+          return React.cloneElement(child, {
+            ...child.props,
+            activeIds,
+            onClick: this.handleChange,
+          });
+        })}
+      </div>
+    );
   }
+  private handleChange = (e, id) => {
+    // const { onChange, accordion } = this.props;
+    // const { activeIds } = this.state;
+    // let newActiveIds = accordion ? [] : activeIds ? [...activeIds] : [];
+    // if (onChange) {
+    //   onChange(id);
+    // }
+    // if (!('activeIds' in this.props)) {
+    //   const index = activeIds.indexOf(id);
+    //   if (index === -1) {
+    //     newActiveIds.push(id);
+    //   } else {
+    //     newActiveIds.splice(index, 1);
+    //   }
+    //   this.setState({
+    //     activeIds: newActiveIds,
+    //   });
+    // }
+  };
 }
 
 export default Collapse;
--- src/components/Collapse/typing.ts
+++ src/components/Collapse/typing.ts
@@ -1,1 +1,10 @@
-export interface CollapseProps extends KUI.BaseProps {}
+export interface CollapseProps extends KUI.BaseProps {
+  activeIds?: string[];
+  defaultActiveIds?: string[];
+  accordion?: boolean;
+  onChange?: (id: string) => void;
+}
+
+export interface CollapseState {
+  activeIds?: string[];
+}
