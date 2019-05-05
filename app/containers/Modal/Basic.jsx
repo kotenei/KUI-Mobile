b73f0630251ec2ef14d4e54dee15cba028978549
kotenei <kotenei@qq.com>
2019-05-05 22:08:29 +0800

    update

--- app/containers/Modal/Basic.jsx
+++ app/containers/Modal/Basic.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { Message, Button, Icon } from 'kui-mobile';
+import { Message, Button, Icon, Modal } from 'kui-mobile';
 
 export default class Demo extends Component {
+  state = {
+    open: false,
+  };
+  handleOpen = () => {
+    this.setState({
+      open: true,
+    });
+  };
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
   render() {
+    const { open } = this.state;
     return (
       <React.Fragment>
-        <Button>提示框</Button>
+        <Button onClick={this.handleOpen}>提示框</Button>
+        <Modal title="标题" content="这里是内容" onCancel={this.handleClose} open={open} />
       </React.Fragment>
     );
   }
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
 import classnames from 'classnames';
 import { ModalProps, ModalState } from './typing';
+import { Button, ButtonGroup } from '../Button';
 
 let seed = 1;
 let zIndex = 1000;
@@ -16,6 +17,7 @@ class Modal extends PureComponent<ModalProps, ModalState> {
     okText: '确定',
     cancelText: '取消',
   };
+
   private static getDerivedStateFromProps(nextProps, prevState) {
     if (nextProps.open) {
       zIndex += 2;
@@ -31,7 +33,9 @@ class Modal extends PureComponent<ModalProps, ModalState> {
 
     return null;
   }
+
   private id: number;
+
   constructor(props) {
     super(props);
     this.id = seed++;
@@ -60,27 +64,85 @@ class Modal extends PureComponent<ModalProps, ModalState> {
   }
 
   public render() {
-    const { className, mask } = this.props;
+    const { className, mask, title, content, cancelText, okText, style } = this.props;
     const { open } = this.state;
-    const classString = classnames({
-      [prefixCls]: true,
-    });
+    const classString = classnames(
+      {
+        [prefixCls]: true,
+        [`${prefixCls}--in`]: open,
+      },
+      className,
+    );
     const maskClassString = classnames({
       [`${prefixCls}-mask`]: true,
-      in: mask && open,
+      [`${prefixCls}-mask--in`]: !!mask && open,
     });
+    const _style = { zIndex, ...style };
+
     return ReactDOM.createPortal(
       <div id={`${this.id}`}>
-        <div className={classString} ref="modal">
-          <div className={`${prefixCls}__header`} ref="header" />
-          <div className={`${prefixCls}__body`} ref="body" />
-          <div className={`${prefixCls}__footer`} ref="footer" />
+        <div className={classString} ref="modal" style={_style}>
+          <div className={`${prefixCls}__header`} ref="header">
+            {title}
+          </div>
+          <div className={`${prefixCls}__body`} ref="body">
+            {content}
+          </div>
+          <div className={`${prefixCls}__footer`} ref="footer">
+            <Button className={`${prefixCls}__cancel`} onClick={this.handleCnacel}>
+              {cancelText}
+            </Button>
+            <Button className={`${prefixCls}__ok`}>{okText}</Button>
+          </div>
         </div>
-        <div />
+        <div
+          className={maskClassString}
+          style={{ zIndex: zIndex - 1 }}
+          onClick={this.handleMaskClick}
+        />
       </div>,
       document.body,
     );
   }
+
+  private handleCnacel = e => {
+    const { onCancel } = this.props;
+    if (onCancel) {
+      onCancel();
+    }
+  };
+
+  private handleOK = e => {
+    const { onOK } = this.props;
+    if (onOK) {
+      onOK();
+    }
+  };
+
+  private handleMaskClick = e => {
+    const { maskClose, onCancel } = this.props;
+    if (maskClose) {
+      this.handleCnacel(e);
+    }
+  };
+
+  private close = () => {
+    if (!this.state.open) {
+      return;
+    }
+    this.setState({
+      open: false,
+    });
+  };
+
+  private open = () => {
+    if (this.state.open) {
+      return;
+    }
+    this.setState({
+      open: true,
+    });
+  };
 }
 
 export default Modal;
